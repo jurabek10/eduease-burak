10556eea757ed0e7c08a4cec1cce6488352e57bd
jurabek10 <samiev.jurabek@bk.ru>
2024-09-21 04:13:56 +0900

    feat: pblish newCourses components

--- src/screens/homePage/NewCourses.tsx
+++ src/screens/homePage/NewCourses.tsx
@@ -1,9 +1,64 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Box, Card, Container, CssVarsProvider, Stack } from "@mui/material";
+
+const list = [
+  {
+    courseName: "Java",
+    imagePath: "/img/onlineLearners.jpg",
+    courseMentor: "Martin",
+    courseDesc: "Good course",
+  },
+  {
+    courseName: "Java",
+    imagePath: "/img/onlineLearners.jpg",
+    courseMentor: "Martin",
+    courseDesc: "Good course",
+  },
+  {
+    courseName: "Java",
+    imagePath: "/img/onlineLearners.jpg",
+    courseMentor: "Martin",
+    courseDesc: "Good course",
+  },
+  {
+    courseName: "Java",
+    imagePath: "/img/onlineLearners.jpg",
+    courseMentor: "Martin",
+    courseDesc: "Good course",
+  },
+];
 export default function NewCourses() {
   return (
-    <div>
-      <Container>NewCourses</Container>
+    <div className={"popular-courses-frame"}>
+      <Container>
+        <Stack className={"popular-section"}>
+          <Box className={"category-title"}>New Courses</Box>
+          <Stack className={"cards-frame"}>
+            {list.map((ele, index) => {
+              return (
+                <CssVarsProvider key={index}>
+                  <div className="popular-course">
+                    <img
+                      className="popular-course-img"
+                      src={ele.imagePath}
+                      alt=""
+                      width={295}
+                      height={200}
+                    />
+                    <div className="popular-course-detail-wrapper">
+                      <h4 className="popular-course-title">{ele.courseName}</h4>
+                      <p className="popular-course-desc">{ele.courseDesc}</p>
+                      <p className="popular-course-mentor">
+                        {ele.courseMentor}
+                      </p>
+                    </div>
+                  </div>
+                </CssVarsProvider>
+              );
+            })}
+          </Stack>
+        </Stack>
+      </Container>
     </div>
   );
 }
