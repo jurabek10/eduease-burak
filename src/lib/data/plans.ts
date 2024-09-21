25527e1705bd7e6f2de4c62b5fe0147d142ed867
jurabek10 <samiev.jurabek@bk.ru>
2024-09-22 04:15:53 +0900

    feat: publish freeCourses component

---
+++ src/lib/data/plans.ts
@@ -0,0 +1,34 @@
+export const plans = [
+  {
+    title: "Hot Discount Days",
+    desc: "Each Friday we deliver highest discounts",
+    author: "Chef Deming",
+    date: "30 min ago",
+    location: "Turkey, Istambul",
+    img: "/img/onlineLearners.jpg",
+  },
+  {
+    title: "Coming Soon",
+    desc: "Be ready for fresh Arrivals from Monday!",
+    author: "Belissimo Arthur",
+    date: "2 days ago",
+    location: "Turkey, Istambul",
+    img: "/img/onlineLearners.jpg",
+  },
+  {
+    title: "New Project Lunch",
+    desc: "New Restaurant is opening in Florida",
+    author: "Morgan News",
+    date: "5 days ago",
+    location: "USA, Florida",
+    img: "/img/onlineLearners.jpg",
+  },
+  {
+    title: "Join us on sns",
+    desc: " We are offering healthy food reciepies on sns",
+    author: "Belissimo Agent",
+    date: "A week ago",
+    location: "Europe, France",
+    img: "/img/onlineLearners.jpg",
+  },
+];
