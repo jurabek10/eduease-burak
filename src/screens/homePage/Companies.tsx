c8c05a441930c44a89a3dfd7ad26c282dc7add79
jurabek10 <samiev.jurabek@bk.ru>
2024-09-22 03:57:01 +0900

    feat: publish companies components

--- src/screens/homePage/Companies.tsx
+++ src/screens/homePage/Companies.tsx
@@ -1,9 +1,35 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
 export default function Companies() {
   return (
-    <div>
-      <Container>Companies</Container>
+    <div className="companies">
+      <Container>
+        <Stack className={"companies-wrapper"}>
+          <Stack className={"companies-top"}>
+            <Box className={"companies-title"}>
+              Trusted by over 1,000 companies and millions of learners around
+              the world
+            </Box>
+          </Stack>
+          <Stack className={"companies-list"}>
+            <Box className={"company"}>
+              <img src="icons/citi_logo.svg" alt="" />
+            </Box>
+            <Box className={"company"}>
+              <img src="icons/samsung_logo.svg" alt="" />
+            </Box>
+            <Box className={"company"}>
+              <img src="icons/ericsson_logo.svg" alt="" />
+            </Box>
+            <Box className={"company"}>
+              <img src="icons/procter_gamble_logo.svg" alt="" />
+            </Box>
+            <Box className={"company"}>
+              <img src="icons/volkswagen_logo.svg" alt="" />
+            </Box>
+          </Stack>
+        </Stack>
+      </Container>
     </div>
   );
 }
