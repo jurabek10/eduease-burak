1ed0f8326295cfd80e8c5321ddd0fd30d82ab0e8
jurabek10 <samiev.jurabek@bk.ru>
2024-09-26 04:07:24 +0900

    feat: publish chosenCourse components

--- src/screens/coursesPage/index.tsx
+++ src/screens/coursesPage/index.tsx
@@ -2,14 +2,28 @@ import Address from "./Address";
 import CourseList from "./CourseList";
 import TopMentors from "./TopMentors";
 import "../../css/course.css";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+import ChosenProduct from "./ChosenCourse";
 
 // import { Container } from "@mui/material";
 export default function CoursesPage() {
+  const courses = useRouteMatch();
   return (
     <div className={"courses-page"}>
-      <CourseList />
-      <TopMentors />
-      <Address />
+      <Switch>
+        <Route path={`${courses.path}/:courseId`}>
+          <ChosenProduct />
+        </Route>
+        <Route path={`${courses.path}`}>
+          <CourseList />
+        </Route>
+        <Route path={`${courses.path}`}>
+          <TopMentors />
+        </Route>
+        <Route path={`${courses.path}`}>
+          <TopMentors />
+        </Route>
+      </Switch>
     </div>
   );
 }
