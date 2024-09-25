4f37b6088b66cce93c830e9a86279a1c20f553fd
jurabek10 <samiev.jurabek@bk.ru>
2024-09-26 03:11:20 +0900

    feat: publish topMentors compoenets

--- src/screens/coursesPage/TopMentors.tsx
+++ src/screens/coursesPage/TopMentors.tsx
@@ -1,3 +1,51 @@
+import { Box, Container, Stack } from "@mui/material";
+
 export default function TopMentors() {
-  return <div className={"coursespage"}></div>;
+  return (
+    <div className={"top-mentors"}>
+      <Container>
+        <Box className="top-mentors-title">Top Mentors</Box>
+        <Stack className="top-mentors-wrapper">
+          <ul className="top-mentors-list">
+            <li className="mentor">
+              <img
+                className="mentor-img"
+                src="/img/jonas.jpeg"
+                alt=""
+                width={300}
+              />
+              <h3 className="mentor-name">Jonas Schmedtmann</h3>
+            </li>
+            <li className="mentor">
+              <img
+                className="mentor-img"
+                src="/img/jonas.jpeg"
+                alt=""
+                width={300}
+              />
+              <h3 className="mentor-name">Jonas Schmedtmann</h3>
+            </li>
+            <li className="mentor">
+              <img
+                className="mentor-img"
+                src="/img/jonas.jpeg"
+                alt=""
+                width={300}
+              />
+              <h3 className="mentor-name">Jonas Schmedtmann</h3>
+            </li>
+            <li className="mentor">
+              <img
+                className="mentor-img"
+                src="/img/jonas.jpeg"
+                alt=""
+                width={300}
+              />
+              <h3 className="mentor-name">Jonas Schmedtmann</h3>
+            </li>
+          </ul>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
